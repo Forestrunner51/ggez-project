281b0158637fe49f59a01c552505e7b1f2d6a4b9
Ola’s Laptop <olaslaptop@Olass-MacBook-Air.local>
2026-04-22 19:35:39 -0500

    coding the code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use ggez::*;
+
+struct State {}
+impl ggez::event::EventHandler for State {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+}
+
+pub fn main() {
+    let state = State {};
+    let c = conf::Conf::new();
+    let (ctx, event_loop) = ContextBuilder::new("hello_ggez", "awesome_person")
+        .default_conf(c)
+        .build()
+        .unwrap();
 }
